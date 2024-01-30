ee0ffe49a1bfb943ae49cad1f9a309fce9751bb7
Павел <45313368+123POG123@users.noreply.github.com>
2024-01-30 17:09:14 +0300

    Update settings.py

--- sitemen/settings.py
+++ sitemen/settings.py
@@ -150,11 +150,7 @@ AUTHENTICATION_BACKENDS = [
 # Send E-mail
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'vinogradovpavel32@gmail.com'
-EMAIL_HOST_PASSWORD = "kxkz jboz klie dhlr"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
 
 AUTH_USER_MODEL = 'users.User'
 
